770996abe0e1ca6caeb64c805ebc83a1a1f2c6a5
stefan dili <stefan.dili.021@gmail.com>
2020-08-19 11:28:58 +0200

    added remove button

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+const formButton = document.createElement("button");
+formButton.innerText = "Add New Book";
+document.body.insertAdjacentElement("beforeend", formButton);
+const bookForm = document.createElement("form");
+bookForm.innerHTML = `
+  <label htmlFor="title">title</label>
+  <input type="text" name="title" id="title"/>
+  <label htmlFor="Author">Author</label>
+  <input type="text" name="Author" id="author"/>
+  <label htmlFor="Rating">Rating</label>
+  <input type="number" min="1" max="5" name="rating" id="rating"/>
+  <label htmlFor="isRead">Have you read it?</label>
+  <input type="checkbox" name="isRead" id="isRead"/>
+  <label htmlFor="pages">How many pages?</label>
+  <input type="number" min="1" max="10000" name="pages" id="pages"/>
+  <button type="submit">Add</button>
+`;
+
 const myLibrary = [];
 const libraryEl = document.querySelector(".my-library");
 
@@ -23,13 +41,14 @@ function createDOMLibrary() {
     .map(
       book => `
   <div class="book-card">
-  <ul>
-    <li>${book.title}</li>
-    <li>by: ${book.author}</li>
-    <li>Rating:${book.rating}</li>
-    <li>Already read: ${book.isRead}</li>
-    <li>${book.pages} pages</li>
+    <ul>
+      <li>${book.title}</li>
+      <li>by: ${book.author}</li>
+      <li>Rating:${book.rating}</li>
+      <li>Already read: ${book.isRead}</li>
+      <li>${book.pages} pages</li>
     </ul>
+    <button id="remove-book" onclick="this.closest('.book-card').remove()" >X</button>
   </div>
   `
     )
@@ -39,6 +58,12 @@ function createDOMLibrary() {
 function render() {
   libraryEl.innerHTML = createDOMLibrary();
 }
+
+function renderForm() {
+  document.body.appendChild(bookForm);
+}
+
+formButton.addEventListener("click", renderForm);
 
 addBookToLibrary(book1);
 addBookToLibrary(book2);
